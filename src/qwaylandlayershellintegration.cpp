826c3e3ce04a0e46843d6fbdffa18e75206be93f
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2021-11-13 12:27:06 +0200

    Port to QtWaylandClient's shell surface api

--- src/qwaylandlayershellintegration.cpp
+++ src/qwaylandlayershellintegration.cpp
@@ -22,26 +22,30 @@ QWaylandLayerShellIntegration::~QWaylandLayerShellIntegration()
 {
 }
 
-bool QWaylandLayerShellIntegration::initialize(QtWaylandClient::QWaylandDisplay *display)
+bool QWaylandLayerShellIntegration::initialize()
 {
-    QWaylandShellIntegration::initialize(display);
-    display->addRegistryListener(registryLayer, this);
-    return m_layerShell != nullptr;
+    if (m_layerShell) {
+        return true;
+    }
+    wl_registry *registry;
+    uint32_t id;
+    uint32_t version;
+    const bool found = findGlobal(QLatin1String("zwlr_layer_shell_v1"), &registry, &id, &version);
+    if (!found) {
+        qWarning() << "Could not find the zwlr_layer_shell_v1 global. Compositor does not support the layer shell protocol?";
+        return false;
+    }
+
+    m_layerShell.reset(new QWaylandLayerShell(registry, id, std::min(version, 4u)));
+    return true;
 }
 
 QtWaylandClient::QWaylandShellSurface *QWaylandLayerShellIntegration::createShellSurface(QtWaylandClient::QWaylandWindow *window)
 {
-    return m_layerShell->createLayerSurface(window);
-}
-
-void QWaylandLayerShellIntegration::registryLayer(void *data, struct wl_registry *registry, uint32_t id, const QString &interface, uint32_t version)
-{
-    QWaylandLayerShellIntegration *shell = static_cast<QWaylandLayerShellIntegration *>(data);
-
-    if (interface == zwlr_layer_shell_v1_interface.name)
-        shell->m_layerShell.reset(new QWaylandLayerShell(registry, id, std::min(version, 4u)));
+    if (m_layerShell) {
+        return m_layerShell->createLayerSurface(window);
+    }
+    return nullptr;
 }
 
 }
-
-//#include "qwaylandlayershellintegration.moc"
