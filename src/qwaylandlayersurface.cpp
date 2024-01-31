6427176da4870ed351fb061cd0a0eaa0f3cccee5
David Edmundson <kde@davidedmundson.co.uk>
2024-01-31 15:17:09 +0000

    Fix build with Qt 6.7
    This also fixes a hypothetical case of a layer shell with subsurfaces
    and matches the call made by xdgshell after the first configure.

--- src/qwaylandlayersurface.cpp
+++ src/qwaylandlayersurface.cpp
@@ -96,7 +96,11 @@ void QWaylandLayerSurface::zwlr_layer_surface_v1_configure(uint32_t serial, uint
     if (!m_configured) {
         m_configured = true;
         window()->resizeFromApplyConfigure(m_pendingSize);
+#if QT_VERSION < QT_VERSION_CHECK(6, 7, 0)
         window()->handleExpose(QRect(QPoint(), m_pendingSize));
+#else
+        window()->sendRecursiveExposeEvent();
+#endif
     } else {
         // Later configures are resizes, so we have to queue them up for a time when we
         // are not painting to the window.
